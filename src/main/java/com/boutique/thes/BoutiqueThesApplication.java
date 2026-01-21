2dcee430e4506a4a8132b104ffc21fe133af9ca1
itsaam <118670879+itsaam@users.noreply.github.com>
2026-01-21 11:39:07 +0100

    Ajout de la classe principale Spring Boot

---
+++ src/main/java/com/boutique/thes/BoutiqueThesApplication.java
@@ -0,0 +1,13 @@
+package com.boutique.thes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoutiqueThesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoutiqueThesApplication.class, args);
+    }
+
+}
