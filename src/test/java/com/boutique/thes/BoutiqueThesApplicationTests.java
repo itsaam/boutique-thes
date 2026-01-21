39576f388259703dabe549cca750361732927b3a
itsaam <118670879+itsaam@users.noreply.github.com>
2026-01-21 11:39:14 +0100

    Ajout des classes de tests

---
+++ src/test/java/com/boutique/thes/BoutiqueThesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boutique.thes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoutiqueThesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
